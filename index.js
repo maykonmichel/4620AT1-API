e0cd9275e5b906b84e7375d48b9a526729c848d0
maykonmichel <maykonmichelpalma@gmail.com>
2020-06-02 11:31:46 -0300

    Configured graphql server

---
+++ index.js
@@ -0,0 +1,6 @@
+import { GraphQLServer } from 'graphql-yoga'
+
+import resolvers from './resolvers';
+
+const server = new GraphQLServer({ typeDefs: 'schema.graphql', resolvers });
+server.start(() => console.log('Server is running on localhost:4000'));
