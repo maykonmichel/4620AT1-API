e95ba2f94ddbbfa01236b3762a10a988aa533aa3
maykonmichel <maykonmichelpalma@gmail.com>
2020-06-02 15:00:30 -0300

    Added db to context

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import {GraphQLServer} from 'graphql-yoga';
+import {Pool} from 'pg';
 
 import resolvers from './resolvers';
 
-new GraphQLServer({typeDefs: 'schema.graphql', resolvers})
+const db = new Pool();
+
+new GraphQLServer({
+  typeDefs: 'schema.graphql',
+  resolvers,
+  context: () => ({db}),
+})
   .start(() => console.log('Server is running on localhost:4000')) // eslint-disable-line no-console
   .then();
