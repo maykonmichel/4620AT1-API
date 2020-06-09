c44be1cbf86a5048ca9e4a989112f10c57a955cc
maykonmichel <maykonmichelpalma@gmail.com>
2020-06-09 07:58:32 -0300

    Added insert movie script

--- scripts/movies.js
+++ scripts/movies.js
@@ -1,5 +1,10 @@
 export default {
   select: `
-    SELECT * FROM movies;
+      SELECT *
+      FROM movies;
+  `,
+  insert: `
+      INSERT INTO movies(name, genre, rating, price)
+      VALUES ($1, $2, $3, $4)
   `,
 };
