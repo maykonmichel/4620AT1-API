788ad32d001ab3d1b710f23370e2c7ff881ea8a4
Gabriel <gabriel.mello@unesp.br>
2020-06-23 20:16:34 -0300

    Primeira versão Script SQL

--- scripts/4620at1.sql
+++ scripts/4620at1.sql
@@ -3,11 +3,46 @@ DROP TABLE IF EXISTS movies;
 DROP TABLE IF EXISTS customer;
 DROP TABLE IF EXISTS people;
 
+
+CREATE TABLE people
+(
+    number INT
+        CONSTRAINT people_pk
+            PRIMARY KEY,
+    name   CHARACTER VARYING,
+    rg     CHARACTER VARYING
+);
+
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE employee
+(
+    number INT
+        CONSTRAINT employee_people_fk references people (number),
+    pis    INT,
+    active BOOLEAN DEFAULT TRUE,
+    admission date,
+    user_access CHARACTER VARYING,
+    password    CHARACTER VARYING
+);
+
+CREATE TABLE customer
+(
+    id  SERIAL
+        CONSTRAINT customer_pk
+            PRIMARY KEY
+        CONSTRAINT customer_people_id_fk
+            REFERENCES people ON DELETE CASCADE,
+    cpf CHARACTER VARYING
+);
+
+
+DROP TABLE IF EXISTS movies;
 CREATE TABLE movies
 (
-    id        SERIAL
-        CONSTRAINT movies_pk
-            PRIMARY KEY,
+ id        SERIAL
+ CONSTRAINT movies_pk
+           PRIMARY KEY,
     name      CHARACTER VARYING,
     genre     CHARACTER VARYING,
     rating    INT,
@@ -26,22 +61,32 @@ CREATE TABLE medias
     available BOOLEAN DEFAULT TRUE
 );
 
-CREATE TABLE people
-(
-    id   SERIAL
-        CONSTRAINT people_pk
-            PRIMARY KEY,
-    name CHARACTER VARYING,
-    rg   CHARACTER VARYING
-);
 
-CREATE TABLE customer
+
+DROP TABLE IF EXISTS rent;
+
+CREATE TABLE rent
 (
-    id  SERIAL
-        CONSTRAINT customer_pk
-            PRIMARY KEY
-        CONSTRAINT customer_people_id_fk
-            REFERENCES people ON DELETE CASCADE,
-    cpf CHARACTER VARYING
-);
+    id     SERIAL
+        CONSTRAINT rent_pk
+            PRIMARY KEY,
+    data_aluguel  DATE,
+    data_devolucao DATE,
+    number INT
+    CONSTRAINT employee_people_fk references employee (number),
+    number INT
+    CONSTRAINT custumer_people_fk references custumer (number),
+    number INT
+    CONSTRAINT _people_fk references people (number),
+
+)
+
+
+
+
+
+
+
+
+>>>>>>> Primeira versão Script SQL
 
